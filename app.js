117ed280bd05962c0281ff32e20292ec315e8910
jmorales113 <jmorales113@gmail.com>
2020-07-27 17:55:25 -0700

    Converted callbacks to promises

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// HTTP (Hypertext Transfer Protocol)
-// Request - What do we want to do
-// Response - What was actually done
-
 const puzzleElement = document.querySelector("#puzzle")
 const guessesElement = document.querySelector("#guesses")
 const game1 = new Hangman("car parts", 2)
@@ -16,31 +12,15 @@ window.addEventListener("keypress", (e) => {
     guessesElement.textContent = game1.statusMessage
 })
 
-getPuzzle((error, puzzle) => {
-    if (error) {
-        console.log(`Error: ${error}`)
-    } else {
-        console.log(puzzle)
-    }
-   
+getPuzzle("2").then((puzzle) => {
+    console.log(puzzle)
+}, (err) => {
+    console.log(`Error: ${err}`)
 })
 
-// Making an HTTP request
+getCountryName("NI").then((country) => {
+    console.log(`Country name: ${country}`)
+}, (err) => {
+    console.log(`Error: ${err}`)
+})
 
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener("readystatechange", (e) => {
-//     const countryCode = "US"
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         data.find((country) => {
-//             if (country.alpha2Code === countryCode) {
-//                 console.log(country.name)
-//             }
-//         })
-//     } else if (e.target.readyState === 4) {
-//         console.log("An error has taken place")
-//     }
-// })
-// countryRequest.open("GET", "http://restcountries.eu/rest/v2/all")
-// countryRequest.send()
--- requests.js
+++ requests.js
@@ -1,16 +1,35 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest()
 
     request.addEventListener("readystatechange", (e) => {
         if (e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.responseText)
-            callback(undefined, data.puzzle)
+            resolve(data.puzzle)
         } else if (e.target.readyState === 4) {
-            callback("An error has taken place", undefined)
+            reject("An error has taken place")
         }
     })
 
-    request.open("GET", "http://puzzle.mead.io/puzzle?wordCount=3")
+    request.open("GET", `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     request.send()
+})
 
-}
+const getCountryName = (countryCode) => new Promise((resolve, reject) => {
+    const countryRequest = new XMLHttpRequest()
+
+    countryRequest.addEventListener("readystatechange", (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText)
+            data.find((country) => {
+                if (country.alpha2Code === countryCode) {
+                    resolve(country.name)
+                }
+            })
+        } else if (e.target.readyState === 4) {
+            reject("An error has taken place")
+        }
+    })
+
+    countryRequest.open("GET", `http://restcountries.eu/rest/v2/`)
+    countryRequest.send()
+})
