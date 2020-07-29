ec8069b8a0ebbbfd6c7d59ba29f005a9e0e6b3f0
jmorales113 <jmorales113@gmail.com>
2020-07-28 20:50:07 -0700

    App is functional; need to style

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const puzzleElement = document.querySelector("#puzzle")
 const guessesElement = document.querySelector("#guesses")
-const game1 = new Hangman("car parts", 2)
-
-puzzleElement.textContent = game1.puzzle
-guessesElement.textContent = game1.statusMessage
+let game1 
 
 window.addEventListener("keypress", (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
-    puzzleElement.textContent = game1.puzzle
-    guessesElement.textContent = game1.statusMessage
+    render()
 })
 
-getPuzzle("2").then((puzzle) => {
-    console.log(puzzle)
-}).catch((error) => {
-    console.log(`Error: ${error}`)
+const render = () => {
+    puzzleElement.textContent = game1.puzzle
+    guessesElement.textContent = game1.statusMessage
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle("2")
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
+
+document.querySelector("#reset").addEventListener("click", () => {
+    startGame()
 })
+
+startGame()
+
+
+// getPuzzle("2").then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((error) => {
+//     console.log(`Error: ${error}`)
+// })
 
 // getCurrentCountry().then((country) => {
 //     console.log(country.name)
